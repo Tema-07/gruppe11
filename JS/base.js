8ec50f4ae0b51bbedb2f03f3e346a4c671f99ce5
My <idla0003@stud.kea.dk>
2025-02-11 14:16:53 +0100

    index og java

---
+++ JS/base.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("#header");
+
+// header.addEventListener("click", minfunktion)
+
+header.addEventListener("click", () => console.log("brugeren har klikket på heading elementet")
+);
+
+function minfunktion(){
+    console.log("brugeren har klikket på heading elementet");
+}
